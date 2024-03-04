11fa10c1791c3166842351fbaf4c79ca84b63d19
Steven Nguyen <snguyen8@uci.edu>
2024-03-03 20:01:21 -0800

    put back semicolon

--- src/com/chess/BlackWidow.java
+++ src/com/chess/BlackWidow.java
@@ -5,6 +5,6 @@ import com.chess.gui.Table;
 public class BlackWidow {
 
     public static void main(final String[] args) throws Exception {
-        Table.get().show()
+        Table.get().show();
     }
 }
